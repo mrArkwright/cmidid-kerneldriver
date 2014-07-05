a3e936a942d9b59c7c5cc2515e21547783f2a0b3
Michael Opitz <michael.w.opitz@gmail.com>
2014-07-05 20:30:20 +0200

    Added new module parameter.

--- module/start_module.sh
+++ module/start_module.sh
@@ -2,7 +2,7 @@
 
 aconnect -x
 rmmod cmidid
-insmod cmidid.ko
+insmod cmidid.ko requested_gpios=22,4,345,435,1,2
 aconnect 24:0 128:0
 
 # MODNAME=$(cat cmidid_main.h | grep "#define MODULE_NAME" | awk '{print $3}' | sed -e 's/"//g')
