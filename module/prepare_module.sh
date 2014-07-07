b896fc5c2ef10a7ddc9342012fdd3ded27cf9c4c
Michael Opitz <michael.w.opitz@gmail.com>
2014-07-07 08:57:38 +0200

    Added more documentation and fixed an error in the presentation.

--- module/prepare_module.sh
+++ module/prepare_module.sh
@@ -2,8 +2,12 @@
 
 # Run this script once after restarting the raspberry pi.
 
+# We need some of the dependencies of the snd_virmidid module.
+# These are made available by loading and unloading this module.
 modprobe snd_virmidi
 rmmod snd_virmidi
 
+# FLUID Synth can be used to sample the MIDI output of our module on
+# the Raspberry Pi.
 killall fluidsynth
 fluidsynth -is -g 5 -a alsa /home/pi/TimGM6mb.sf2 &
